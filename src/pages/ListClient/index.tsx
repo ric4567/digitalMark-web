69d0a30b25d3c47bdf2dd02c4d39d41dddcc3e4c
Ricardo Reinert <ricardorkarnopp@gmail.com>
2023-03-06 10:00:58 -0300

    feat: botão de edit e loading na lista de clientes

--- src/pages/ListClient/index.tsx
+++ src/pages/ListClient/index.tsx
@@ -2,21 +2,24 @@ import { Link } from "gatsby-link";
 import React, { useState, useEffect } from "react";
 import api from "../../api";
 import Layout from "../../components/layout";
-import "./styles.css";
+import LoadingRing from "../../components/loadingRing";
 
 const ListClient = () => {
   const [data, setData] = useState<ClientModel[]>([]);
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
     try {
+      setLoading(true);
       var response = await api.get(`/api/client`);
       setData(response.data);
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   };
 
   return (
@@ -29,14 +32,26 @@ const ListClient = () => {
           <th>Nome</th>
           <th>Tecnologia</th>
           <th>Projeto</th>
+          <th></th>
         </tr>
-        {data.map((c) => (
+        {loading ? (
           <tr>
-            <td>{c.name}</td>
-            <td>{c.technology}</td>
-            <td>{c.projectName}</td>
+            <LoadingRing />
           </tr>
-        ))}
+        ) : (
+          data.map((c) => (
+            <tr>
+              <td>{c.name}</td>
+              <td>{c.technology}</td>
+              <td>{c.projectName}</td>
+              <td>
+                <Link to={`/SaveClient?id=${c.id}`}>
+                  <i className="bx bxs-edit text-xl"></i>
+                </Link>
+              </td>
+            </tr>
+          ))
+        )}
       </table>
     </Layout>
   );
