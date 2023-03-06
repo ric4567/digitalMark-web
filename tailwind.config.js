2c1444483ec035e080a628e569b5def6d4b42ff4
Ricardo Reinert <ricardorkarnopp@gmail.com>
2023-03-06 07:15:09 -0300

    feat: configurações iniciais do gatsby

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/index.tsx",
+    "./src/components/navigation.tsx",
+    "./src/components/layout.tsx",
+    "./src/components/header.tsx",
+    "./src/pages/SaveClient/index.tsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
